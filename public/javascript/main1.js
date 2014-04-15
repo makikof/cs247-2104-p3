1fba22f8a262595f27fa16806829334efdc225cc
amberr <rrockw@gmail.com>
2014-04-14 20:27:45 -0700

    fixed url even more

--- public/javascript/main1.js
+++ public/javascript/main1.js
@@ -37,7 +37,7 @@
     }else{
       fb_chat_room_id = Math.random().toString(36).substring(7);
     }
-    display_msg({m:"Share this url with your friend to join this chat: "+ document.location.origin+"?"+fb_chat_room_id,c:"red"})
+    display_msg({m:"Share this url with your friend to join this chat: "+ document.location.origin+"/proto1?"+fb_chat_room_id,c:"red"})
 
     // set up variables to access firebase data structure
     var fb_new_chat_room = fb_instance.child('chatrooms').child(fb_chat_room_id);
